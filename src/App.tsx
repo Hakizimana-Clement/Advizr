0239ce6a086a61f461f827b24c6c910e3736dfbd
Hakizimana Clement <h.clementoert@gmail.com>
2024-10-23 18:43:51 +0200

    ft(App.tsx): Complete Design of Adviz by build card and add loading loader for new advice loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
-      inventore, delectus cumque beatae autem culpa amet maiores sequi,
-      assumenda, ut repellat. Dolores consectetur consequuntur fugit ipsa
-      ratione non aspernatur eligendi?
-    </p>
+    <>
+      <p>UI Loading...</p>
+    </>
   );
 }
 
