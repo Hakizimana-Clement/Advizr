108f94a60610a1179738cbcc4b6a00cf61cf87f4
Hakizimana Clement <h.clementoert@gmail.com>
2024-10-30 15:02:38 +0200

    ft(src/): Clean up src folder by removing unused files

--- src/newIdeas/addQueryParameter.js
+++
@@ -1,6 +0,0 @@
-// ############### Update url query params ##################
-export const updateURL = (key, value) => {
-  const url = new URL(window.location.href);
-  url.searchParams.set(key, value);
-  window.history.replaceState({}, document.title, url.href);
-};
