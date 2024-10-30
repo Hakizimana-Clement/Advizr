96ae15e3ec6365f72ddd10571bc42ed855b03525
Hakizimana Clement <h.clementoert@gmail.com>
2024-10-30 16:47:45 +0200

    feat(AdviceApi.ts): Integrate advice fetching from api.adviceslip.com using React Query

--- src/components/Layouts/Card.tsx
+++ src/components/Layouts/Card.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import ScaleLoader from "react-spinners/ScaleLoader";
+import { AdviceApi } from "../../api/AdviceApi";
 import {
   dice_button,
   pattern_divider_desktop,
@@ -6,16 +9,43 @@ import {
 } from "../../utils/Images";
 
 const Card = () => {
+  const {
+    data: advices,
+    isLoading,
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryKey: ["advices"],
+    queryFn: AdviceApi,
+    enabled: false,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  const handleClick = async () => {
+    refetch();
+  };
+
   return (
     <>
       <main className="flex justify-center">
         <div className="bg-D_gray-Dark_grayish_blue text-center py-6 px-5 rounded-lg relative w-[93%] sm:w-[65%] md:w-[55%] lg:w-[50%] xl:w-[34%]">
-          <p className="text-D_gray-Neon_green text-base tracking-widest md:text-lg">
-            Advice #<span>53</span>
-          </p>
-          <h2 className="text-D_gray-Ligth_cyan text-2xl my-8 mx-1 font-Manrope">
-            "Accentuate the positive, eliminate the negative."
-          </h2>
+          {isLoading ? (
+            <>
+              <ScaleLoader color="#fff" />
+            </>
+          ) : (
+            <>
+              <p className="text-D_gray-Neon_green text-base tracking-widest md:text-lg">
+                Advice #<span>{advices?.id}</span>
+              </p>
+              <h2 className="text-D_gray-Ligth_cyan text-2xl my-8 mx-1 font-Manrope">
+                {advices?.advice}
+              </h2>
+            </>
+          )}
 
           {/* Divider */}
           <div className="mt-5 mb-10 flex justify-center lg:hidden">
@@ -32,9 +62,20 @@ const Card = () => {
           </div>
 
           {/* Random button */}
-          <div className="absolute inset-x-2 -bottom-10">
-            <button className="p-5 bg-D_gray-Neon_green rounded-full transform transition hover:shadow-glow">
-              <img src={dice_button} alt="random button" />
+          <div
+            className="absolute inset-x-2 -bottom-10 cursor-pointer"
+            onClick={handleClick}
+          >
+            <button
+              className={
+                "p-5 bg-D_gray-Neon_green rounded-full transform transition hover:shadow-glow"
+              }
+            >
+              <img
+                src={dice_button}
+                alt="random button"
+                className={isFetching ? "rotate" : ""}
+              />
             </button>
           </div>
         </div>
