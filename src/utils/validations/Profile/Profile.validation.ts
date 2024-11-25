6aa07e1e38b4518df82399d3dc48787b00bf8b12
Hakizimana Clement <h.clementoert@gmail.com>
2024-11-25 22:54:02 +0200

    git merge ft-user-setting-form -m "Merged ft-user-setting-form into develop. Closes #20"

--- src/utils/validations/Profile/Profile.validation.ts
+++ src/utils/validations/Profile/Profile.validation.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const ProfileSchema = z.object({
   profileImage: z.string().url().optional(),
   username: z.string().optional(),
+
   email: z.string().optional(),
 });
 
